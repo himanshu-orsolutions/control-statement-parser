e2f19cd5d3e4c3d2910ad10640949f6b36353917
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2020-04-27 17:37:21 +0530

    fix logs

--- src/main/java/com/parse/TaskExecutor.java
+++ src/main/java/com/parse/TaskExecutor.java
@@ -813,7 +813,6 @@ public class TaskExecutor {
 				codeBuilder.append(line);
 				codeBuilder.append("\n");
 			}
-			System.out.println(codeBuilder.toString());
 			String formattedUpdatedCode = gooleFormatter.formatSource(codeBuilder.toString());
 			saveUpdatedCode(formattedUpdatedCode, inputFilePath);
 
