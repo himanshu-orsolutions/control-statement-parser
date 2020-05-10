9e05527e467affefb49e926ce8a6d4a3e676d5a4
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2020-05-10 07:32:47 +0530

    remove lofs

--- src/main/java/com/parse/TaskExecutor.java
+++ src/main/java/com/parse/TaskExecutor.java
@@ -997,7 +997,6 @@ public class TaskExecutor {
 				codeBuilder.append(line);
 				codeBuilder.append("\n");
 			}
-			System.out.println(codeBuilder.toString());
 			String formattedUpdatedCode = gooleFormatter.formatSource(codeBuilder.toString());
 			saveUpdatedCode(formattedUpdatedCode, inputFilePath);
 
